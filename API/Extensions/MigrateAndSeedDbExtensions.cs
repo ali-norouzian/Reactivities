4e2472819e8dbf40c9ac04fbb50ffc2ab295c132
ali-norouzian <ali.norouzian@outlook.com>
2023-09-13 16:18:00 +0330

    Register and Login with jwt and some accounting ad

--- API/Extensions/MigrateAndSeedDbExtensions.cs
+++ API/Extensions/MigrateAndSeedDbExtensions.cs
@@ -1,4 +1,6 @@
 
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -13,9 +15,10 @@ namespace API.Extensions
             try
             {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
 
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
             }
             catch (Exception ex)
             {
--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,23 @@
 using API.Controllers.Middlewares;
 using API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+                                .RequireAuthenticatedUser()
+                                .Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 
 builder.Services.ApplicationServices(builder.Configuration, builder.Environment);
+
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -24,6 +34,7 @@ app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
