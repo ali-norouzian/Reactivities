18a6d6204d9962b41e93615db69b48816316359b
ali-norouzian <ali.norouzian@outlook.com>
2023-09-13 16:28:56 +0330

    Some error handling feature improved

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -41,7 +41,19 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await _userManager.Users.AnyAsync(u => u.UserName == registerDto.Username))
-                return BadRequest("Username is already taken");
+            {
+                ModelState.AddModelError("username", "Username taken");
+
+                // Its return normalize format obj return and has our errors
+                return ValidationProblem(ModelState);
+            }
+
+            if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+            {
+                ModelState.AddModelError("email", "Email taken");
+
+                return ValidationProblem(ModelState);
+            }
 
             var user = new AppUser
             {
